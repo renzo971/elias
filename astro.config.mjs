ccdca90b7c52d4a8b1590c8b2fe5303f53945089
Renzo Santos <72644376+renzo971@users.noreply.github.com>
2026-05-09 20:04:33 -0500

    fix: agregamos el paquete de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+
+  integrations: [react()],
+  adapter: vercel()
 });
